a2fbe0a61eb3da547cfa0973decb70dfd19204e7
Gabriel Remiszewski <Gabriel.Remiszewski@outlook.de>
2024-10-19 11:05:02 +0200

    2 + 3

--- Exercise 1/main.py
+++ Exercise 1/main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import typing
 
 
 def generate_points(pairs: int, experiments: int = 1) -> np.array:
@@ -22,3 +24,17 @@ def calculate_pi(**kwargs) -> np.array:
         radii = (kwargs.get("points") ** 2).sum(axis=0)
     inside_the_circle = (radii <= 1)
     return 4 * inside_the_circle.sum(axis=0) / len(radii)
+
+
+def generate_hist_for_pi(pi_array: np.array, pairs: int, experiments: int, bins: int = 10, show: bool = True,
+                         save_path: typing.Optional[str] = None
+                         ) -> None:
+    pi_mean = pi_array.mean(axis=0)
+    pi_std = pi_array.std(axis=0)
+    plt.hist(pi_array, bins=10)
+    plt.title(f"$P = {pairs} X = {experiments}$")
+    plt.axvline(pi_mean, color='r')
+    plt.axvline(pi_mean + pi_std, color='r', linestyle='--')
+    plt.axvline(pi_mean - pi_std, color='r', linestyle='--')
+    plt.show()
+    # TODO Save
