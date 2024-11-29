19272f4dc0eb8b23192a78750eee3575c21b7cfd
Gabriel Remiszewski <Gabriel.Remiszewski@outlook.de>
2024-11-29 09:52:35 +0100

    fft

--- CompPhy/autocorrelation.py
+++ CompPhy/autocorrelation.py
@@ -1,23 +1,28 @@
 import numpy as np
+from numpy.typing import NDArray
 
 
-def normalized_autocorrelation(time_series: np.ndarray, mean: float | None = None) -> np.ndarray:
+def normalized_autocorrelation(time_series: np.ndarray, mean: float | None = None, fast: bool = True) -> np.ndarray:
 
     if mean is None:
         mean = time_series.mean()
 
     series: np.ndarray = time_series - mean
 
-    C: np.ndarray = np.zeros_like(time_series)
+    if not fast:
+        C: np.ndarray = np.zeros_like(time_series)
 
-    n = len(series)
-    for t in range(n - 1):
-        C[t] = (series[: n - t] * series[t:]).mean()
+        n = len(series)
+        for t in range(n - 1):
+            # C[t] = (series[: n - t] * series[t:]).mean()
+            C[t] = (series * np.roll(series, shift=t, axis=0)).mean()
+    else:
+        C = np.fft.ifft(np.fft.fft(time_series) * np.fft.ifft(time_series)).real
 
     return C / C[0]
 
 
-def integrated_autocorrelation(time_series: np.ndarray, mean: float | None = None) -> int:
+def integrated_autocorrelation(time_series: np.ndarray, mean: float | None = None, fast: bool = True) -> int:
     # until sign flip
 
     # for i in range(2, len(correlation)):
@@ -28,7 +33,7 @@ def integrated_autocorrelation(time_series: np.ndarray, mean: float | None = Non
 
     # return tau
 
-    C = normalized_autocorrelation(time_series, mean)
+    C = normalized_autocorrelation(time_series, mean, fast)
 
     try:
         first_zero = np.where(C <= 0)[0][0]
@@ -37,9 +42,9 @@ def integrated_autocorrelation(time_series: np.ndarray, mean: float | None = Non
         return len(C)  # at least!
 
 
-def blocking(time_series: np.ndarray, mean: float | None = None, tau: float | None = None):
+def blocking(time_series: np.ndarray, mean: float | None = None, tau: float | None = None, fast: bool = True):
     if tau is None:
-        tau = int(np.ceil(integrated_autocorrelation(time_series, mean)))
+        tau = int(np.ceil(integrated_autocorrelation(time_series, mean, fast)))
 
     n = len(time_series) // tau
 
@@ -51,4 +56,14 @@ def blocking(time_series: np.ndarray, mean: float | None = None, tau: float | No
     return blocked
 
 
-__all__ = ["normalized_autocorrelation", "integrated_autocorrelation", "blocking"]
+def mean_spin_spin_correlation(cfgs: NDArray[np.int8]) -> NDArray[np.int8]:
+    """
+
+    Calculate the mean of the spin-spin two-point correlation function using the fast 
+    fourier transform algorithm
+
+    """
+    return np.fft.ifft2(np.fft.fft2(cfgs) * np.fft.ifft2(cfgs)).real.mean(axis=0)
+
+
+__all__ = ["normalized_autocorrelation", "integrated_autocorrelation", "blocking", "mean_spin_spin_correlation"]
